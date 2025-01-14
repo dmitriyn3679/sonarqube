b1232fee1934a13f17da87e6f61e7dbc8498df82
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-01-14 14:39:47 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Counter from './components/Counter.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Counter />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
