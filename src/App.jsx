a23a4b66e14fb655fee670865b136fcd5832b731
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-01-14 15:05:26 +0200

    add husky and 2 components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Counter from './components/Counter.jsx';
+import BadPracticeComponent from "./components/BadPracticeComponent.jsx";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Counter />
+      <BadPracticeComponent />
       <Footer />
     </div>
   );
