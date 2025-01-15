1ad7dab8f47e0a53ae7e7d1ae61910f32d2a948b
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-01-15 10:03:59 +0200

    number bug

--- src/components/DuplicateComponent1.jsx
+++ src/components/DuplicateComponent1.jsx
@@ -2,6 +2,15 @@
 import React from 'react';
 
 const DuplicateComponent1 = () => {
+  
+  const test = () => {
+    number = 10;
+    
+    return number;
+  };
+  
+  console.log(test())
+  
   return (
     <div>
       <p>This is a duplicated component.</p>
